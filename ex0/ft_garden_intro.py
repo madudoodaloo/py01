715668b15fb7944f0ebd1ead9fd40c89ef82ea05
masilva- <masilva-@student.42lisboa.com>
2026-08-24 16:02:06 +0100

    classes, instances and methods

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro() -> None:
+	name: str = "Dalia"
+	height: int = 10
+	age: int = 30
+
+	print("=== Welcome to the Garden ===")
+	print("Plant:", name)
+	print(f"Height: {height}cm")
+	print("Age:", age, "days")
+	print("=== End of Program ===")
+
+
+
+if __name__ == "__main__":
+	ft_garden_intro()
---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,28 @@
+#!/bin/usr/env pyhton3
+
+class Plant:
+	def __init__(self, name: str, height: int, age: int) -> None:
+		self.name: str = name
+		self.height: int = height
+		self.age: int = age
+
+	def show(self) -> None:
+		print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+def main() -> None:
+	print("=== Garden Plant Registry ===")
+
+	# Instantiating Plant 1
+	plant1 = Plant("Rose", 25, 30)
+	plant1.show()
+
+	# Instantiating Plant 2
+	plant2 = Plant("Sunflower", 80, 45)
+	plant2.show()
+
+	# Instantiating Plant 3
+	plant3 = Plant("Cactus", 15, 120)
+	plant3.show()
+
+if __name__ == "__main__":
+	main()
